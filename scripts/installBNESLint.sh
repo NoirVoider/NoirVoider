9611bc8a83d149a9620af3fab911e2e01cb0b3f4
唐天博 <gd523718@gmail.com>
2023-03-13 18:17:51 +0800

    feat: 关闭 no-param-reassign

--- scripts/installBNESLint.sh
+++ scripts/installBNESLint.sh
@@ -216,7 +216,8 @@ module.exports = {
     camelcase: 0,
     "spaced-comment": 0,
     "import/order": 0,
-    "no-unused-vars": 0
+    "no-unused-vars": 0,
+    "no-param-reassign": 0
     // "off" 或 0 - 关闭规则
     // "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
     // "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
